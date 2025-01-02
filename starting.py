985ef41f006cb9472dd4f7f1bac791fc49a76b2f
Slonetech <kengeregordon@gmail.com>
2025-01-02 16:51:35 +0300

    Ternary operators

--- starting.py
+++ starting.py
@@ -1,8 +1,13 @@
 greeting ="Hello, World!"
 print(greeting)
 
-meaning = 42
-print(meaning)
+meaning = 48
+# print(meaning)
 
-if meaning > 10:
-    print("This is a big number.")
+# if meaning > 10:
+#     print("This is a big number.")
+# else:
+#     print("This is a small number.")
+
+# Ternary operator
+print ("this is a big number") if meaning >10 else print ("small number")
